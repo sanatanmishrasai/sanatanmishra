f4e1fae9859ac1fc74e6157cb33180c884a71a92
sanatan mishra <sanatanmishrasai@gmail.com>
2024-10-14 01:00:51 +0530

    The scroler animation is done 1/2 Done

--- script/script.js
+++ script/script.js
@@ -2,3 +2,13 @@ const nav = document.getElementById('ul3');
 function showNav(nav_value) {
     nav.style.display = nav_value;
 }
+
+
+function selector(s_num){
+    const selectorall = document.querySelectorAll('.scrole_element');
+    let length_selector = selectorall.length;
+    for(var i = 0; i < length_selector; i++){
+        selectorall[i].setAttribute('src', 'images/select false.svg');
+    }
+    document.getElementById(s_num).setAttribute('src', 'images/select true.svg');
+}
